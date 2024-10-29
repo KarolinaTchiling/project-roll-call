bcc01019c35dca4863add00891c690da004b862e
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-29 12:16:54 -0400

    02--Imported routes, themeing and navbar into app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 import SummaryPage from './pages/SummaryPage';
+import TodayPage from './pages/TodayPage';
+import YesterdayPage from './pages/YesterdayPage';
+import HistoryPage from './pages/HistoryPage';
+import InsightsPage from './pages/InsightsPage';
+
+
 import DBport from './pages/DBport';
-// import Navbar from './components/Navbar';
-// import TestPage from './pages/TestPage';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<SummaryPage />} />
-        <Route path="/DBport" element={<DBport />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<SummaryPage />} />
+          <Route path="/today" element={<TodayPage />} />
+          <Route path="/yesterday" element={<YesterdayPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/insights" element={<InsightsPage />} />
+
+          <Route path="/DBport" element={<DBport />} />
+        </Routes>
+
+    </ThemeProvider>
   );
 }
 
