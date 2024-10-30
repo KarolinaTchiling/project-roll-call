bfd8461e4fe80d979be37fd2bd55c5fce74915a5
Joshua Huliganga <jhuliganga2000@gmail.com>
2024-10-30 19:38:11 -0400

    03--Get upcoming events for today from Google API and display them

--- backend/flask_app/app.py
+++ backend/flask_app/app.py
@@ -1,41 +1,47 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
-from quickstart import get_ten_events
+from quickstart import *
 
 app = Flask(__name__)
 
 # enable CORS so that React can communicate with Flask
 CORS(app)
 
-# set up MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client["appDB"]
-users = db["users"]
+# # set up MongoDB
+# client = MongoClient("mongodb://localhost:27017/")
+# db = client["appDB"]
+# users = db["users"]
 
-# helper function to convert db document ID into a string
-def convert_id_to_string(doc):
-    doc['_id'] = str(doc['_id'])
-    return doc
+# # helper function to convert db document ID into a string
+# def convert_id_to_string(doc):
+#     doc['_id'] = str(doc['_id'])
+#     return doc
 
-# route for getting data from db
-@app.route("/api/data", methods=['GET'])
-def get_data():
-    data = [convert_id_to_string(item) for item in users.find()]
-    return jsonify(data)
+# # route for getting data from db
+# @app.route("/api/data", methods=['GET'])
+# def get_data():
+#     data = [convert_id_to_string(item) for item in users.find()]
+#     return jsonify(data)
 
-# route for adding new data into the db
-@app.route("/api/data", methods=['POST'])
-def post_data():
-    new_data = request.json
-    users.insert_one(new_data)
-    return jsonify(convert_id_to_string(new_data)), 201
+# # route for adding new data into the db
+# @app.route("/api/data", methods=['POST'])
+# def post_data():
+#     new_data = request.json
+#     users.insert_one(new_data)
+#     return jsonify(convert_id_to_string(new_data)), 201
 
-# route for getting the 10 upcoming events from Google Calendar api
-@app.route("/api/events", methods=['GET'])
-def get_events():
-    events = get_ten_events()
+# route for getting the upcoming events from today from Google Calendar api
+@app.route("/day_events", methods=['GET'])
+def get_day_events():
+    events = get_day()
     return jsonify(events)
+
+# route for getting the upcoming events within a week from Google Calendar api
+# @app.route("/week_events", methods=['GET'])
+# def get_week_events():
+#     events = get_week()
+#     return jsonify(events)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- backend/flask_app/quickstart.py
+++ backend/flask_app/quickstart.py
@@ -6,22 +6,60 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import pytz
 
-# If modifying these scopes, delete the file token.json.
+# if modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
+# def get_week():
+#     creds = None
+#     if os.path.exists("token.json"):
+#         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 "credentials.json", SCOPES
+#             )
+#             creds = flow.run_local_server(port=0)
+#         with open("token.json", "w") as token:
+#             token.write(creds.to_json())
 
-def get_ten_events():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+#     try:
+#         service = build("calendar", "v3", credentials=creds)
+
+#         print("Getting the upcoming events for the week...")
+        
+#         # set now to the time right now, and time_max to the time 7 days from now
+#         now = dt.datetime.now(dt.UTC)
+#         now_iso = now.isoformat()
+#         time_max = (now + dt.timedelta(days=7)).isoformat()
+
+#         events_result = (
+#             service.events().list(
+#                 calendarId="primary",
+#                 timeMin=now_iso,
+#                 timeMax=time_max,
+#                 singleEvents=True,
+#                 orderBy="startTime",
+#             ).execute()
+#         )
+
+#         events = events_result.get("items", [])
+
+#         if not events:
+#             print("No weekly events...")
+
+#         return events
+    
+#     except HttpError as error:
+#         print(f"An error occurred: {error}")
+
+def get_day():
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -30,38 +68,33 @@ def get_ten_events():
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
         service = build("calendar", "v3", credentials=creds)
 
-        # Call the Calendar API
-        now = dt.datetime.now(dt.UTC).isoformat()
+        # set now to the time right now, and end_of_day to the time at the end of today
+        timezone = pytz.timezone("America/New_York")
+        now = dt.datetime.now(timezone)
+        now_iso = now.isoformat()
+        end_of_day = now.replace(hour=23, minute=59, second=59, microsecond=999999).isoformat()
 
-        print("Getting the upcoming 10 events")
+        print("Getting today's events")
         
-        events_result = (
-            service.events().list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            ).execute()
-        )
-
+        events_result = service.events().list(
+            calendarId="primary",
+            timeMin=now_iso,
+            timeMax=end_of_day,
+            singleEvents=True,
+            orderBy="startTime"
+        ).execute()
+        
         events = events_result.get("items", [])
 
         if not events:
-            print("No upcoming events found.")
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
+            print("No events found for today.")
 
         return events
     
