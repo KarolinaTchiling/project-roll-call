c67df50b64774616eeb91d43726506a1cf7cea37
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-02 14:04:18 -0400

    06--implemented a function to format time

---
+++ frontend/src/utility/dateUtils.ts
@@ -0,0 +1,14 @@
+
+export function formatTime(dateTime: string): string {
+
+    const date = new Date(dateTime);
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+  
+    hours = hours % 12;
+    hours = hours ? hours : 12; // Adjust 0 hours to 12
+    const minutesStr = minutes < 10 ? `0${minutes}` : minutes; // Pad minutes
+  
+    return `${hours}:${minutesStr} ${ampm}`;
+}
