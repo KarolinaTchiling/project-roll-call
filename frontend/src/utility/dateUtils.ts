a737c5cfba8506c9b281a0ff8d79c7f3bd1e1d30
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-07 19:47:29 -0500

    Updated PB with new story points and merged stories

--- frontend/src/utility/dateUtils.ts
+++ frontend/src/utility/dateUtils.ts
@@ -1,4 +1,5 @@
 
+// a function which formats the time string returned from the google api to a more beautiful one: (11:00 AM)
 export function formatTime(dateTime: string): string {
 
     const date = new Date(dateTime);
