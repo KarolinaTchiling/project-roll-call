4e7c72dd39fd55e993fca925debcce8a7349ba3a
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-12 14:51:15 -0500

    Removed unused imports

--- frontend/src/components/FutureBubble.tsx
+++ frontend/src/components/FutureBubble.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CalendarEvent } from '../types';
-import { formatTime } from '../utility/dateUtils';
 import { getEventType } from '../utility/eventUtils';
 
 function FutureBubble() {
