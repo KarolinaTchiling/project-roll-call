3b3ce83d7d1d29a72d0277b60f7a71eb345defe9
Abhinaya <abhisonukondi123@gmail.com>
2024-11-25 13:14:21 -0500

    21--Blue background container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import './index.css';
 
+import DashboardPage from './pages/DashboardPage';
 import LandingPage from './pages/LandingPage';
 import TodayPage from './pages/TodayPage';
 import YesterdayPage from './pages/YesterdayPage';
@@ -18,6 +19,7 @@ function App() {
     <div className="h-screen bg-custombg">
     <ThemeProvider theme={theme}>
         <Routes>
+          <Route path="/dashboard" element={<DashboardPage/>} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/today" element={<TodayPage />} />
           <Route path="/yesterday" element={<YesterdayPage />} />
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import '../DashboardPage.css';
 import Navbar from '../components/Navbar';
 
 const DashboardPage = () => {
   return (
-    <div>
-      <Navbar /> 
-      Welcome to Dashboard page 
-    </div>
+    <>
+        <div className="flex flex-col min-w-[800px] bg-custombg">
+            <Navbar />
+            <div className="container">
+                <div className="container-center"></div>
+            </div>
+        </div>
+    </>  
+    
   )
 }
 
