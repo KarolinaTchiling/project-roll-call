80cf4dc01a0141252faba7f33cf885a3de38cb8e
Abhinaya <abhisonukondi123@gmail.com>
2024-11-25 14:10:11 -0500

    21--heading text on dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -8,7 +8,11 @@ const DashboardPage = () => {
         <div className="flex flex-col min-w-[800px] bg-custombg">
             <Navbar />
             <div className="container">
-                <div className="container-center"></div>
+                <div className="container-center">
+                    <div className="text">
+                    Sara's Dashboard
+                    </div>
+                </div>
             </div>
         </div>
     </>  
