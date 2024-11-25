73e042644d89476e636942735f7299bd78df30b1
Abhinaya <abhisonukondi123@gmail.com>
2024-11-25 11:36:35 -0500

    21--created new dashboard page

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <Navbar /> 
+      Welcome to Dashboard page 
+    </div>
+  )
+}
+
+export default DashboardPage
