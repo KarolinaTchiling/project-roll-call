090903ef556ee97e9fcde8a8d217241be518f3fa
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-29 21:02:06 -0500

    Updated testing route

--- backend/app/routes/reports.py
+++ backend/app/routes/reports.py
@@ -1,34 +1,13 @@
-from ..services.report_service import gmail_send_message, create_report
+from ..services.report_service.email_service import gmail_send_message
 from . import report
-from flask import session
-from flask import request, jsonify
-import requests
 
-
-
+# this is just a testing route as this task is automated 
 @report.route("/send_self", methods=['GET'])
 def send_self():
-    user_email = session.get("user", {}).get("email")
-    gmail_send_message(user_email)
+    gmail_send_message("saras.rollcall@gmail.com", "110125061098661425360")
     return "check inbox and logs"
 
-@report.route("/send_other/<other_email>", methods=['GET'])
-def send_other(other_email):
-    gmail_send_message(other_email)
-    return f"Check {other_email}'s inbox and logs"
 
-@report.route("/test", methods=['POST'])
-def test():
-    # Get the Bearer token from the Authorization header
-    token = request.headers.get("Authorization")
-    if not token:
-        return jsonify({"message": "Token is missing!"}), 403
-
-    # Remove "Bearer " prefix if present
-    token = token.split(" ")[1] if " " in token else token
-
-    # Call create_report with the token
-    return create_report(token)
 
 
 
