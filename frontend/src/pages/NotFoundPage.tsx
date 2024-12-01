fdafd5fdca98ed67d6fd137fbd6188799d8f751b
Karolina Tchiling <kdt@my.yorku.ca>
2024-12-01 15:35:04 -0500

    28--Added link to landing page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <div className="text-center pt-[200px]">
-      <p className="text-2xl font-bold">404</p>
-      <p>The page you are looking for does not exist.</p>
+    <div className="flex flex-col items-center h-screen text-center pt-[200px]">
+      <div className="text-2xl font-bold">404</div>
+      <div className="mb-4">The page you are looking for does not exist.</div>
+      <Link to="/" className="text-blue-500 hover:underline">
+        Go Back
+      </Link>
     </div>
   );
 };
